4e1f04cb3ff457a8605a0498021b145a72f8eb62
jyee0012 <jyee12@studentmail.nait.ca>
2018-09-15 21:55:23 -0600

    finished madlib

--- DMIT2025/week02/madlib.php
+++ DMIT2025/week02/madlib.php
@@ -73,17 +73,43 @@
 				</div> -->
 		  </div>
 
+		  <div class="form-group">
+				<label for="personality">Personality:</label>
+		    <div class="radio">
+					<label><input type="radio" name="personality" value="polite">Polite</label>
+				</div>
+				<div class="radio">
+					<label><input type="radio" name="personality" value="rude">Rude</label>
+				</div>
+				<div class="radio">
+					<label><input type="radio" name="personality" value="shy">Shy</label>
+				</div>
+				<div class="radio">
+					<label><input type="radio" name="personality" value="kind">Kind</label>
+				</div>
+				<div class="radio">
+					<label><input type="radio" name="personality" value="sarcastic">Sarcastic</label>
+				</div>
+				<!-- <div class="radio">
+					<label><input type="radio" name="gender" value="other">Other</label>
+				</div> -->
+		  </div>
+
 			<div class="form-group">
 				<label for="end">Ending:</label>
 				<select class="form-control" id="end" name="end">
 					<option>happy</option>
 					<option>sad</option>
 					<option>bad</option>
-					<option>funny</option> 
-					<option>game</option> 
 					<option>.....</option>
 				</select>
 			</div>
+
+			
+			<div class="form-group">
+			<label for="hobby">Your Hobby:</label>
+			<input type="text" class="form-control" id="hobby" name="hobby">
+		</div>
 
 		  <input type="submit" class="btn btn-default" name="mysubmit">
 
@@ -95,79 +121,99 @@
 			$color = $_POST["color"];
 			$garment = $_POST["garment"];
 			$gender = $_POST["gender"];
+			$personality = $_POST["personality"];
 			$end = $_POST["end"];
-
+			$hobby = $_POST["hobby"];
+			$bHaveInfo = ($fname != "" && $lname != "" && $gender != "");
 			//echo "$fname, $lname, $color, $garment, $gender";
 			//echo $end;
-			
+			if ($hobby == ""){
+				$hobby = "write stories";
+			}
+
 			switch($gender){
 				case "male":
 				$pref = "girl";
 				$gender = "boy";
+				$playerG = "his";
+				$genderO = "man";
+				$playerS = "He";
 				break;
 				case "female":
 				$pref = "boy";
 				$gender = "girl";
+				$playerG = "her";
+				$genderO = "woman";
+				$playerS = "She";
 				break;
 				default:
 				$pref = "it";
 				break;
 			}
 
+			switch($personality){
+				case "polite":
+				$response = "Thanks for noticing my $garment, and I agree it may be quite ridiculous.";
+				break;
+				case "shy":
+				$response = "Umm......thanks.";
+				break;
+				case "rude":
+				$response = "How dare you ridicule my $garment, it is one of my favorites!";
+				break;
+				case "kind":
+				$response = "I was simply on my way, until I encounter such a beautiful $pref on my stroll.";
+				break;
+				case "sarcastic":
+				$response = "Oh thank you, you are too kind to compliment me on my $garment.";
+				break;
+			}
+			
 			switch($end){
 				case "happy":
-				$ending = "";
+				$ending = "The $pref decided to shakehands and exchange parting words before they split up. <br>";
+				$ending .= "Then $fname, and the $pref lived happily ever after.";
 				break;
 				case "sad":
-				$ending = "";
+				$ending = "The $pref grabbed ahold of $fname's hand and whispers a dark secret into $playerG's ear. <br>";
+				$ending .= "Then $fname cried $playerG heart out for the poor $pref.";
 				break;
 				case "bad":
-				$ending = "";
-				break;
-				case "funny":
-				$ending = "";
+				$ending = "The $pref then suddenly smacked $fname across the face and ran into the night. <br>";
+				$ending .= "The $pref left $fname and never came back or spoke to $fname ever again.";
 				break;
 				case ".....":
 				$ending = "Then all of a sudden $fname woke up.";
+				$end = "a dream";
 				break;
 			}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 			if (isset($_POST['mysubmit']))
 			{
 				//echo "submit";
+				// story needs to have 8 variables and 12 sentences of content.
+				// $fname, $lname, $color, $garment, $gender, $pref, $ending 
 				$story = "Once upon a time, there was a $gender name $fname $lname. One fine day, $fname was out and about wearing a $color $garment and looking for something to do. <br>";
 				
 				$story .= "Then, $fname saw a cute $pref walk by. \"Hey Cutie\" said $fname. <br>";
 				$story .= "The $pref looked in the direction of $fname, and fell down laughing. \"What are you doing wearing that ridiculous $color $garment\" said the $pref. <br>";
-
+				$story .= $response . "<br>";
+				$story .= "$fname and the $pref continued the conversation for hours until the sun went down. <br>";
+				$story .= "While in the middle of the night, people could still hear catter happening. <br>";
+				$story .= "It was $fname and the $pref who were still conversing at night. <br>";
+				$story .= $ending . "<br>";
+				$story .= "<br>";
+				$story .= "There once was a $genderO, who likes to $hobby. <br>";
+				$story .= "$playerS once wrote a story about $fname and a $pref. <br>";
+				$story .= "Where they went a little adventure while $fname wore a $color $garment and $fname was a little bit $personality. <br>";
+				$story .= "The ending of the story was $end. <br>";
+				
 				echo "<br>";
 				echo "<p>";
 				// on your own, finish the story......
-				if ($fname != "" && $lname != "" && $gender != ""){
+				if ($bHaveInfo){
 					echo $story;
-					echo $ending;
 				}else{
 					echo "Please fill in all information above.";
 				}
