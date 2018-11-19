1325f0ce0e6593f3ff0166497dc68eb0e6671102
jyee0012 <jyee12@studentmail.nait.ca>
2018-11-19 16:41:32 -0700

    worked on insert (next work on modify then filter)

--- DMIT2025/week12/catalog/admin/insert.php
+++ DMIT2025/week12/catalog/admin/insert.php
@@ -14,17 +14,16 @@
 		$airing = trim($_POST['airing']);
 		$descrip = trim($_POST['descrip']);
 
-		$gAction = trim($_POST['genre_']);
-		$gAdven = trim($_POST['genre_']);
-		$gComedy = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
-		$gAction = trim($_POST['genre_']);
+		$gAction = trim($_POST['genre_action']);
+		$gAdven = trim($_POST['genre_adventure']);
+		$gComedy = trim($_POST['genre_comedy']);
+		$gFantasy = trim($_POST['genre_fantasy']);
+		$gGame = trim($_POST['genre_game']);
+		$gMagic = trim($_POST['genre_magic']);
+		$gMystery = trim($_POST['genre_mystery']);
+		$gSchool = trim($_POST['genre_school']);
+		$gSports = trim($_POST['genre_sports']);
+		$gSuper = trim($_POST['genre_super']);
 
 		$newfile = is_uploaded_file($_FILES['imgfile']['tmp_name']);
 
@@ -38,23 +37,22 @@
         $episodeL = filter_var($episodeL, FILTER_SANITIZE_NUMBER_INT);
         $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
 		
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);
-        $airing = filter_var($airing, FILTER_SANITIZE_NUMBER_INT);		
+        // $gAction = filter_var($gAction, FILTER_SANITIZE_NUMBER_INT);
+        // $gAdven = filter_var($gAdven, FILTER_SANITIZE_NUMBER_INT);
+        // $gComedy = filter_var($gComedy, FILTER_SANITIZE_NUMBER_INT);
+        // $gFantasy = filter_var($gFantasy, FILTER_SANITIZE_NUMBER_INT);
+        // $gGame = filter_var($gGame, FILTER_SANITIZE_NUMBER_INT);
+        // $gMagic = filter_var($gMagic, FILTER_SANITIZE_NUMBER_INT);
+        // $gMystery = filter_var($gMystery, FILTER_SANITIZE_NUMBER_INT);
+        // $gSchool = filter_var($gSchool, FILTER_SANITIZE_NUMBER_INT);
+        // $gSports = filter_var($gSports, FILTER_SANITIZE_NUMBER_INT);
+        // $gSuper = filter_var($gSuper, FILTER_SANITIZE_NUMBER_INT);
 
 		$boolValidateOK = true;
 		$stringValidate = "";
 		$alertString = "success";
-		$newFilename = "";
-		
+		$newFilename = '';
+
 		if ($newfile){	
 			$filename = $_FILES['imgfile']['name'];
 			$filetempname = $_FILES['imgfile']['tmp_name'];
@@ -107,9 +105,13 @@
 			}
 
 			$sql = "INSERT INTO $database 
-			(jye_series_name, jye_alter_name, jye_series_length, jye_episode_length, jye_airing, jye_series_image, jye_series_source, jye_rating, jye_description, jye_data_source) VALUES 
-			('$seriesN', '$alterN', '$seriesL', '$episodeL', '$airing', '$newFilename', '$seriesSrc', '$rating', '$descrip', '$dataSrc')";
-
+			(jye_series_name, jye_alter_name, jye_series_length, jye_episode_length, jye_airing, jye_series_image, jye_series_source, jye_rating, jye_description, jye_data_source,
+			jye_genre_action, jye_genre_adventure, jye_genre_comedy, jye_genre_fantasy, jye_genre_game, jye_genre_magic, jye_genre_mystery, jye_genre_school, jye_genre_sports, jye_genre_supernatural 
+			) VALUES 
+			('$seriesN', '$alterN', '$seriesL', '$episodeL', '$airing', '$newFilename', '$seriesSrc', '$rating', '$descrip', '$dataSrc',
+			'$gAction', '$gAdven', '$gComedy', '$gFantasy', '$gGame', '$gMagic', '$gMystery', '$gSchool', '$gSports', '$gSuper'
+			)";
+			//echo $sql; 
 			mysqli_query($con, $sql) or die(mysqli_error($con));
 			
 			$stringValidate = "<p>\"$seriesN\" Successfully Uploaded.</p>";
@@ -198,13 +200,13 @@
 					<?php if ($airingValidate){echo "<div class=\"alert alert-warning\">" .$airingValidate. "</div>"; } ?>
 				</div>
 
-
 				<div class="form-group">
 					<label for="descrip">Description:</label>
 					<textarea name="descrip" id="descrip" class="form-control textarea-height" placeholder="Synopsis"><?php if ($descrip) echo $descrip ?></textarea>
 					<?php if ($descripValidate){echo "<div class=\"alert alert-warning\">" .$descripValidate. "</div>"; } ?>
 				</div>
-				<div class="form-check">
+				<div class="form-group" style="padding-bottom: 140px;">
+				<div class="col-md-4">
 					<div class="form-check form-check-inline">
 						<input class="form-check-input" type="checkbox" name="genre_action" id="genre_action" value="1" <?php if ($gAction) echo "checked"; ?>>
 						<label class="form-check-label" for="genre_action">Action</label>
@@ -226,7 +228,8 @@
 						<label class="form-check-label" for="genre_game">Game</label>
 					</div>
 				</div>
-				<div class="form-check">
+				
+				<div class="col-md-4">
 					<div class="form-check form-check-inline">
 						<input class="form-check-input" type="checkbox" name="genre_magic" id="genre_magic" value="1" <?php if ($gMagic) echo "checked"; ?>>
 						<label class="form-check-label" for="genre_magic">Magic</label>
@@ -240,14 +243,16 @@
 						<label class="form-check-label" for="genre_school">School</label>
 					</div>
 					<div class="form-check form-check-inline">
-						<input class="form-check-input" type="checkbox" name="genre_sport" id="genre_sport" value="1" <?php if ($gSports) echo "checked"; ?>>
-						<label class="form-check-label" for="genre_sport">Sports</label>
+						<input class="form-check-input" type="checkbox" name="genre_sports" id="genre_sports" value="1" <?php if ($gSports) echo "checked"; ?>>
+						<label class="form-check-label" for="genre_sports">Sports</label>
 					</div>
 					<div class="form-check form-check-inline">
 						<input class="form-check-input" type="checkbox" name="genre_super" id="genre_super" value="1" <?php if ($gSuper) echo "checked"; ?>>
 						<label class="form-check-label" for="genre_super">Supernatural</label>
 					</div>
 				</div>
+				</div>
+
 				<div class="form-group">
 					<label for="imgfile">Thumbnail Image:</label>
 					<input class="" type="file" name="imgfile">
